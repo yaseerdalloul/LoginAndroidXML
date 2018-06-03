c13b4f39825893ba1d51bab3007f572628a48cff
Yasir Dalloul <31539650+yaseerdalloul@users.noreply.github.com>
2018-06-03 22:24:02 +0300

    Initial commit

---
+++ app/src/main/java/dalloul/yjd/com/test_project/MainActivity.java
@@ -0,0 +1,13 @@
+package dalloul.yjd.com.test_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
